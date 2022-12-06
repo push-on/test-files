153fe8d45829a3753fd84418ec52e744108e2d9c
mhimranhossain <mh.imranhossain@outlook.com>
2022-12-06 20:03:37 +0600

    commit

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,7 @@
 <script>
-  import Test2 from './libs/test2.svelte'
   import Test from './libs/test.svelte'
 </script>
 
 <main>
-  <!-- <Test2 /> -->
   <Test />
 </main>
--- src/libs/test2.svelte
+++
@@ -1,5 +0,0 @@
-<div class="container">
-  <h1 class="underline">hello World</h1>
-  <input type="text" placeholder="Input">
-  <button class="blue">Button</button>
-</div>
